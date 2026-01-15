d365ef15917dec497b11abc75b27f0560daf002f
Rafael <rafaeloliveirasouzasil@gmail.com>
2026-01-15 17:59:08 -0300

    Métodos personalizados de busca

--- src/main/java/com/rafakliv/sistema/clientes/repositories/CustomersRepository.java
+++ src/main/java/com/rafakliv/sistema/clientes/repositories/CustomersRepository.java
@@ -4,9 +4,24 @@ import com.rafakliv.sistema.clientes.models.CustomersModels;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface CustomersRepository extends JpaRepository<CustomersModels, UUID> {
 
+    Optional<CustomersModels> findByEmail(String email);
+
+    CustomersModels findByUuid(UUID uuid);
+
+    CustomersModels findByName(String name);
+
+    List<CustomersModels> findByCountry(String country);
+
+    List<CustomersModels> findByCity(String city);
+
+    List<CustomersModels> findByTypeCustomers(String typeCustomers);
+
+    List<CustomersModels> findByRoad(String road);
 }
