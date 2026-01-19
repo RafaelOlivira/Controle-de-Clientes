bfb766e318d44f988e93baead708e29c817802cc
Rafael <rafaeloliveirasouzasil@gmail.com>
2026-01-19 12:23:51 -0300

    Controle de Rotas do Template

--- src/main/java/com/rafakliv/sistema/clientes/controllers/TemplateController.java
+++ src/main/java/com/rafakliv/sistema/clientes/controllers/TemplateController.java
@@ -1,4 +1,32 @@
 package com.rafakliv.sistema.clientes.controllers;
 
+import com.rafakliv.sistema.clientes.models.CustomersModels;
+import com.rafakliv.sistema.clientes.services.CustomersServices;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/home")
 public class TemplateController {
+
+    @Autowired
+    private CustomersServices customersServices;
+
+    @GetMapping()
+    public String home(Model model){
+        model.addAttribute("customer", new CustomersModels());
+        return "index";
+    }
+    @PostMapping("/new/create")
+    public String createCustomers(@ModelAttribute("customer") CustomersModels customer){
+        customersServices.registerCustomers(customer);
+        return "redirect:/home";
+    }
+
+
 }
