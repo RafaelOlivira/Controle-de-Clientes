cb8cd47a6f745f51a04870d0cf1deb17b22f89f9
Rafael <rafaeloliveirasouzasil@gmail.com>
2026-01-25 19:05:53 -0300

    Ajustes

--- src/main/java/com/rafakliv/sistema/clientes/models/CustomersModels.java
+++ src/main/java/com/rafakliv/sistema/clientes/models/CustomersModels.java
@@ -1,6 +1,9 @@
 package com.rafakliv.sistema.clientes.models;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -20,24 +23,31 @@ public class CustomersModels {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private UUID uuid;
 
+    @NotBlank
     @Column(nullable = false)
     private String name;
 
+    @Min(1)
     @Column(nullable = false)
     private int age;
 
+    @NotBlank
     @Column(nullable = false)
     private String email;
 
+    @NotBlank
     @Column(nullable = false)
     private String typeCustomers;
 
+    @NotBlank
     @Column(nullable = false)
     private String road;
 
+    @NotBlank
     @Column(nullable = false)
     private String country;
 
+    @NotBlank
     @Column(nullable = false)
     private String city;
 
